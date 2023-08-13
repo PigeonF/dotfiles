eeae2eaff5cd98061f164d36bf6b37c3debfe72a
Jonas Fierlings <fnoegip@gmail.com>
2023-08-13 12:48:05 +0200

    fix(zsh): Add Homebrew to path on macOS

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -1,10 +1,19 @@
-{ pkgs, config, ... }: {
+{ pkgs, config, lib, ... }:
+with lib;
+let
+  inherit (pkgs) stdenv;
+in
+{
   programs.zsh = {
     enable = true;
 
     dotDir = ".config/zsh";
 
     history.path = "${config.xdg.cacheHome}/zsh_history.txt";
+
+    profileExtra = optionalString stdenv.isDarwin ''
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+    '';
 
     shellAliases = {
       ls = "exa";
