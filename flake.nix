5991b9accae575ccd823c65bed0dffec467db512
Jonas Fierlings <fnoegip@gmail.com>
2023-08-13 10:07:08 +0200

    refactor(nix): Move kamino configuration into its own file

--- flake.nix
+++ flake.nix
@@ -14,51 +14,23 @@
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
-    let
-      configuration = { pkgs, ... }: {
-        # List packages installed in system profile. To search by name, run:
-        # $ nix-env -qaP | grep wget
-        environment.systemPackages = with pkgs;
-          [
-            vim
-            just
-          ];
-
-        # Auto upgrade nix package and the daemon service.
-        services.nix-daemon.enable = true;
-        # nix.package = pkgs.nix;
-
-        # Necessary for using flakes on this system.
-        nix.settings.experimental-features = "nix-command flakes";
-
-        # Create /etc/zshrc that loads the nix-darwin environment.
-        programs.zsh.enable = true; # default shell on catalina
-        # programs.fish.enable = true;
-
-        # Set Git commit hash for darwin-version.
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-
-        # Used for backwards compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 4;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-      };
-    in
     {
       # Build darwin flake using:
       # $ darwin-rebuild build --flake .#kamino
-      darwinConfigurations."kamino" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-          }
-        ];
-      };
+      darwinConfigurations."kamino" = nix-darwin.lib.darwinSystem
+        {
+          specialArgs = { inherit inputs; };
+
+          modules = [
+            ./hosts/kamino
+
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+            }
+          ];
+        };
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."kamino".pkgs;
