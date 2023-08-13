8e183a761b7986269fdfe927fc520d34172dcc67
Jonas Fierlings <fnoegip@gmail.com>
2023-08-13 10:19:01 +0200

    refactor(nix): Make system settings more immediately visible

--- flake.nix
+++ flake.nix
@@ -15,22 +15,45 @@
 
   outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, ... }:
     {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#kamino
-      darwinConfigurations."kamino" = nix-darwin.lib.darwinSystem
-        {
-          specialArgs = { inherit inputs; };
+      darwinConfigurations."kamino" =
+        let
+          user = "pigeon";
+        in
+        nix-darwin.lib.darwinSystem
+          {
+            specialArgs = { inherit inputs; };
 
-          modules = [
-            ./hosts/kamino
+            modules = [
+              ./hosts/kamino
 
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-            }
-          ];
-        };
+              ({ pkgs, ... }: {
+                nixpkgs.hostPlatform = "aarch64-darwin";
+                users.users.${user} = {
+                  home = "/Users/${user}";
+                  shell = pkgs.zsh;
+                };
+
+                system = {
+                  configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
+                  stateVersion = 4;
+                };
+
+                nix = {
+                  package = pkgs.nixFlakes;
+                  settings = {
+                    allowed-users = [ user ];
+                    experimental-features = [ "nix-command" "flakes" ];
+                  };
+                };
+              })
+
+              home-manager.darwinModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+              }
+            ];
+          };
 
       # Expose the package set, including overlays, for convenience.
       darwinPackages = self.darwinConfigurations."kamino".pkgs;
--- hosts/kamino/default.nix
+++ hosts/kamino/default.nix
@@ -3,12 +3,4 @@
     ./packages.nix
     ./services.nix
   ];
-
-  system = {
-    configurationRevision = inputs.self.rev or inputs.self.dirtyRev or null;
-    stateVersion = 4;
-  };
-
-  nixpkgs.hostPlatform = "aarch64-darwin";
-  nix.settings.experimental-features = "nix-command flakes";
 }
