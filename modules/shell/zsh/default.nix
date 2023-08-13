f57d397fcb2413a19f52d060d2b30fde03ee2d1c
Jonas Fierlings <fnoegip@gmail.com>
2023-08-13 12:21:15 +0200

    refactor(zsh): Install common programs

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -1,5 +1,25 @@
-{
+{ pkgs, config, ... }: {
   programs.zsh = {
     enable = true;
+
+    dotDir = ".config/zsh";
+
+    history.path = "${config.xdg.cacheHome}/zsh_history.txt";
+
+    shellAliases = {
+      ls = "exa";
+      la = "ls -la";
+
+      fda = "fd --no-ignore --hidden";
+      rga = "rg --no-ignore --hidden";
+    };
+  };
+
+  home = {
+    packages = with pkgs; [
+      exa
+      fd
+      ripgrep
+    ];
   };
 }
