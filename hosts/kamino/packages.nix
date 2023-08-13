190034e445b1bd178110e3864b2997e7b69af40d
Jonas Fierlings <fnoegip@gmail.com>
2023-08-13 10:22:51 +0200

    feat(nix): Enable SSH integration for 1password

--- hosts/kamino/packages.nix
+++ hosts/kamino/packages.nix
@@ -18,6 +18,11 @@
   };
 
   programs = {
-    zsh.enable = true;
+    zsh = {
+      enable = true;
+      variables = {
+        SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+      };
+    };
   };
 }
