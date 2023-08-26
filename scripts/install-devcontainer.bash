c242984607771e5d40f74cace8189ccce345e194
Jonas Fierlings <fnoegip@gmail.com>
2023-08-26 12:11:54 +0200

    fix(scripts): Fix install-devcontainer.bash

--- scripts/install-devcontainer.bash
+++ scripts/install-devcontainer.bash
@@ -11,4 +11,6 @@ if ! command -v nix 1>/dev/null; then
     failure "nix not installed. Install with 'sh <(curl -L https://nixos.org/nix/install) --daemon'"
 fi
 
-nix run home-manager/release-23.05 -- switch --flake .#pigeon@devbox
+mkdir -p ~/.local/state/nix/profiles
+
+nix run home-manager/release-23.05 -- switch --flake .#developer@devbox
