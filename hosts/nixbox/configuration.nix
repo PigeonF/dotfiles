02e92b1e589bade10c1a7db1826cc8a94c757a0d
Jonas Fierlings <fnoegip@gmail.com>
2024-01-19 18:26:26 +0100

    feat: Add developer user

--- hosts/nixbox/configuration.nix
+++ hosts/nixbox/configuration.nix
@@ -1,16 +1,16 @@
-{ config, pkgs, ... }:
-
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./hardware-builder.nix
-      ./bootloader.nix
-      ./vagrant.nix
-      ./custom-configuration.nix
-    ];
-
-
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./hardware-builder.nix
+    ./bootloader.nix
+    ./vagrant.nix
+    ./custom-configuration.nix
+  ];
 
   # remove the fsck that runs at startup. It will always fail to run, stopping
   # your boot until you press *.
@@ -20,13 +20,12 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-  services.openssh.extraConfig =
-    ''
-      PubkeyAcceptedKeyTypes +ssh-rsa
-    '';
+  services.openssh.extraConfig = ''
+    PubkeyAcceptedKeyTypes +ssh-rsa
+  '';
 
   # Enable DBus
-  services.dbus.enable    = true;
+  services.dbus.enable = true;
 
   # Replace ntpd by timesyncd
   services.timesyncd.enable = true;
@@ -43,36 +42,53 @@
     rsync
   ];
 
-  users.users.root = { password = "vagrant"; };
+  users.users.root = {password = "vagrant";};
   # Creates a "vagrant" group & user with password-less sudo access
   users.groups.vagrant = {
     name = "vagrant";
-    members = [ "vagrant" ];
+    members = ["vagrant"];
   };
   users.users.vagrant = {
-    description     = "Vagrant User";
-    name            = "vagrant";
-    group           = "vagrant";
-    extraGroups     = [ "users" "wheel" ];
-    password        = "vagrant";
-    home            = "/home/vagrant";
-    createHome      = true;
+    description = "Vagrant User";
+    name = "vagrant";
+    group = "vagrant";
+    extraGroups = ["users" "wheel"];
+    password = "vagrant";
+    home = "/home/vagrant";
+    createHome = true;
     useDefaultShell = true;
     openssh.authorizedKeys.keys = [
-          "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
     ];
     isNormalUser = true;
   };
 
-  security.sudo.extraConfig =
-    ''
-      Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
-      Defaults:root,%wheel env_keep+=NIX_PATH
-      Defaults:root,%wheel env_keep+=TERMINFO_DIRS
-      Defaults env_keep+=SSH_AUTH_SOCK
-      Defaults lecture = never
-      root   ALL=(ALL) SETENV: ALL
-      %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
-    '';
+  security.sudo.extraConfig = ''
+    Defaults:root,%wheel env_keep+=LOCALE_ARCHIVE
+    Defaults:root,%wheel env_keep+=NIX_PATH
+    Defaults:root,%wheel env_keep+=TERMINFO_DIRS
+    Defaults env_keep+=SSH_AUTH_SOCK
+    Defaults lecture = never
+    root   ALL=(ALL) SETENV: ALL
+    %wheel ALL=(ALL) NOPASSWD: ALL, SETENV: ALL
+  '';
 
+  users.groups.developer = {
+    name = "developer";
+    members = ["developer"];
+  };
+  users.users.developer = {
+    description = "Developer";
+    name = "developer";
+    group = "developer";
+    extraGroups = ["users" "wheel"];
+    password = "developer";
+    home = "/home/developer";
+    createHome = true;
+    useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICSGbm3QEVQFhYqJM29rQ6WibpQr613KgxoYTr/QvztV"
+    ];
+    isNormalUser = true;
+  };
 }
