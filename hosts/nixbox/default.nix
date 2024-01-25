b45b718e1e57e9a39a6e3c0f38fd73bc718bb6b3
Jonas Fierlings <fnoegip@gmail.com>
2024-01-25 18:08:26 +0000

    .

--- hosts/nixbox/default.nix
+++ hosts/nixbox/default.nix
@@ -7,9 +7,12 @@ _: {
   ];
 
   virtualisation = {
+    docker = {
+      enable = true;
+    };
+
     podman = {
       enable = true;
-      dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
