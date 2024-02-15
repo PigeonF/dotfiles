453afb6d64c08e58ea0ca50bf2e61ea04dec517a
Jonas Fierlings <fnoegip@gmail.com>
2024-02-15 17:01:21 +0100

    .

--- hosts/nixbox/users/developer/default.nix
+++ hosts/nixbox/users/developer/default.nix
@@ -22,37 +22,39 @@ systemConfig:
         ;
     };
 
-    file.".gitlab-ci-local/buildkitd.default.toml" = {
-      text = ''
-        [registry."${systemConfig.nixbox.registryHost}"]
-          ca=["/etc/docker/certs.d/${systemConfig.nixbox.registryHost}/ca.crt"]
-      '';
-    };
-
-    file.".gitlab-ci-local/ca-bundle.crt" = {
-      text =
-        (builtins.readFile "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt")
-        + "\n"
-        + (builtins.readFile ../../services/docker-registry/minica.pem);
-    };
-
-    file.".gitlab-ci-local/.env" = {
-      text =
-        let
-          home = config.home.homeDirectory;
-          volumes = lib.strings.concatStringsSep " " [
-            "certs:/certs/client"
-            "${home}/.gitlab-ci-local/buildkitd.default.toml:/etc/buildkit/buildkitd.toml:ro"
-            "${home}/.gitlab-ci-local/ca-bundle.crt:/etc/ssl/certs/nixbox-bundle.crt:ro"
-            "/etc/docker/certs.d/:/etc/docker/certs.d/:ro"
-          ];
-          variables = lib.strings.concatStringsSep " " [ "SSL_CERT_FILE=/etc/ssl/certs/nixbox-bundle.crt" ];
-        in
-        ''
-          PRIVILEGED=true
-          VOLUME="${volumes}"
-          VARIABLE="${variables}"
+    file = {
+      ".gitlab-ci-local/buildkitd.default.toml" = {
+        text = ''
+          [registry."${systemConfig.nixbox.registryHost}"]
+            ca=["/etc/docker/certs.d/${systemConfig.nixbox.registryHost}/ca.crt"]
         '';
+      };
+
+      ".gitlab-ci-local/ca-bundle.crt" = {
+        text =
+          (builtins.readFile "${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt")
+          + "\n"
+          + (builtins.readFile ../../services/docker-registry/minica.pem);
+      };
+
+      ".gitlab-ci-local/.env" = {
+        text =
+          let
+            home = config.home.homeDirectory;
+            volumes = lib.strings.concatStringsSep " " [
+              "certs:/certs/client"
+              "${home}/.gitlab-ci-local/buildkitd.default.toml:/etc/buildkit/buildkitd.toml:ro"
+              "${home}/.gitlab-ci-local/ca-bundle.crt:/etc/ssl/certs/nixbox-bundle.crt:ro"
+              "/etc/docker/certs.d/:/etc/docker/certs.d/:ro"
+            ];
+            variables = lib.strings.concatStringsSep " " [ "SSL_CERT_FILE=/etc/ssl/certs/nixbox-bundle.crt" ];
+          in
+          ''
+            PRIVILEGED=true
+            VOLUME="${volumes}"
+            VARIABLE="${variables}"
+          '';
+      };
     };
 
     sessionVariables = {
