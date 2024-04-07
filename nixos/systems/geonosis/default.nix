5077c61618b33057a43c9f63af0c8f8ed446a2d8
Jonas Fierlings <fnoegip@gmail.com>
2024-04-07 21:09:01 +0200

    chore(nix): Overwrite geonosis ssh keys

--- nixos/systems/geonosis/default.nix
+++ nixos/systems/geonosis/default.nix
@@ -1,25 +1,33 @@
 { inputs, ... }:
 
 {
-  flake.nixosModules.geonosis = _: {
-    imports = [
-      inputs.self.nixosModules.home-manager
-      inputs.disko.nixosModules.disko
+  flake.nixosModules.geonosis =
+    { lib, ... }:
+    let
+      sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILt6md+QmEdseqB8kRqrsTIOvGsph+um1Lb5xijO7Vfj";
+    in
+    {
+      imports = [
+        inputs.self.nixosModules.home-manager
+        inputs.disko.nixosModules.disko
 
-      inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480s
+        inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t480s
 
-      inputs.self.nixosModules.sshRoot
-      inputs.self.nixosModules.pigeon
-      ./disk.nix
-    ];
+        inputs.self.nixosModules.sshRoot
+        inputs.self.nixosModules.pigeon
+        ./disk.nix
+      ];
 
-    boot.loader.grub = {
-      devices = [ "/dev/nvme0" ];
+      boot.loader.grub = {
+        devices = [ "/dev/nvme0" ];
 
-      efiSupport = true;
-      efiInstallAsRemovable = true;
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
+
+      system.stateVersion = "24.05";
+
+      users.users.pigeon.openssh.authorizedKeys.keys = lib.mkForce [ sshKey ];
+      users.users.root.openssh.authorizedKeys.keys = lib.mkForce [ sshKey ];
     };
-
-    system.stateVersion = "24.05";
-  };
 }
