6bf4b5f11cd5eb1a06ac9bd4f4421c5960a35070
Jonas Fierlings <fnoegip@gmail.com>
2024-04-08 20:32:54 +0200

    chore(nix): Add gitlab-ci-local .env file

--- home-manager/configs/gitlab-ci-local.nix
+++ home-manager/configs/gitlab-ci-local.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
   home = {
@@ -6,6 +6,24 @@
 
     sessionVariables = {
       GCL_ARTIFACTS_TO_SOURCE = "false";
+    };
+
+    file = {
+      ".gitlab-ci-local/.env" = {
+        text =
+          let
+            volumes = lib.strings.concatStringsSep " " [
+              "certs:/certs/client"
+              "/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro"
+            ];
+            variables = lib.strings.concatStringsSep " " [ "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt" ];
+          in
+          ''
+            PRIVILEGED=true
+            VOLUME="${volumes}"
+            VARIABLE="${variables}"
+          '';
+      };
     };
   };
 }
