1937ecc90e562db7482f70b09ac51bf5bb2ce0e3
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 11:46:55 +0200

    chore(nix): Enable all terminfo packages

--- nixos/core.nix
+++ nixos/core.nix
@@ -12,6 +12,10 @@ _:
         system = {
           configurationRevision = lib.mkDefault (inputs.self.rev or inputs.self.dirtyRev or null);
         };
+
+        environment = {
+          enableAllTerminfo = true;
+        };
       };
   };
 }
