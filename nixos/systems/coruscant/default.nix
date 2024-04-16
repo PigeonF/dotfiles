2e454b773a4cc7ea11b2367032fdfa922c65d391
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 14:58:25 +0200

    chore(nix): Add script to relay ssh-agent

--- nixos/systems/coruscant/default.nix
+++ nixos/systems/coruscant/default.nix
@@ -22,6 +22,29 @@
         defaultUser = "pigeon";
       };
 
-      environment.defaultPackages = [ pkgs.socat ];
+      environment.defaultPackages = [
+        (pkgs.writeShellApplication {
+          name = "relay-openssh-agent";
+
+          runtimeInputs = [ pkgs.socat ];
+
+          text = ''
+            mkdir -p /mnt/d/tmp
+
+            export SSH_AUTH_SOCK=/mnt/d/tmp/ssh-agent.sock
+            ALREADY_RUNNING=$(pgrep -f "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; true)
+            if [[ -z "$ALREADY_RUNNING" ]]; then
+                if [[ -S $SSH_AUTH_SOCK ]]; then
+                    echo "removing previous socket..."
+                    rm $SSH_AUTH_SOCK
+                fi
+                echo "Starting SSH-Agent relay..."
+                (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+            else
+                echo "SSH-Agent relay is already running: $ALREADY_RUNNING"
+            fi
+          '';
+        })
+      ];
     };
 }
