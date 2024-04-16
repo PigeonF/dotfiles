9473642c85bd13c9ae2549e7c2e6d67da8e67a35
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 13:52:18 +0200

    chore(nix): Fix flake check

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -6,13 +6,15 @@ _:
     ssh =
       { lib, ... }:
       {
-        services.openssh.enable = lib.mkDefault true;
-        services.openssh.openFirewall = lib.mkDefault true;
-        services.openssh.extraConfig = ''
-          AcceptEnv LANG LANGUAGE LC_*
-          AcceptEnv COLORTERM TERM TERM_*
-          AcceptEnv WEZTERM_REMOTE_PANE
-        '';
+        services.openssh = {
+          enable = lib.mkDefault true;
+          openFirewall = lib.mkDefault true;
+          extraConfig = ''
+            AcceptEnv LANG LANGUAGE LC_*
+            AcceptEnv COLORTERM TERM TERM_*
+            AcceptEnv WEZTERM_REMOTE_PANE
+          '';
+        };
 
         security.sudo.extraConfig = ''
           Defaults env_keep+=SSH_AUTH_SOCK
--- nixos/systems/coruscant/default.nix
+++ nixos/systems/coruscant/default.nix
@@ -1,23 +1,21 @@
 { inputs, ... }:
 
 {
-  flake.nixosModules.coruscant =
-    { config, ... }:
-    {
-      imports = [
-        inputs.nixos-wsl.nixosModules.wsl
+  flake.nixosModules.coruscant = _: {
+    imports = [
+      inputs.nixos-wsl.nixosModules.wsl
 
-        inputs.self.nixosModules.core
-        inputs.self.nixosModules.home-manager
-        inputs.self.nixosModules.nix
-        inputs.self.nixosModules.pigeon
-      ];
+      inputs.self.nixosModules.core
+      inputs.self.nixosModules.home-manager
+      inputs.self.nixosModules.nix
+      inputs.self.nixosModules.pigeon
+    ];
 
-      networking.hostName = "coruscant";
+    networking.hostName = "coruscant";
 
-      wsl = {
-        enable = true;
-        defaultUser = "pigeon";
-      };
+    wsl = {
+      enable = true;
+      defaultUser = "pigeon";
     };
+  };
 }
