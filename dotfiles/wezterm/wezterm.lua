fa9fea9e7ec8ecf9a59a6ba468ae76313b702bcb
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 11:51:01 +0200

    chore(wezterm): Set TERM variable

--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -1,8 +1,7 @@
-local wezterm = require("wezterm")
-local shells = require("shells")
+local wezterm = require 'wezterm'
+local shells = require 'shells'
 
 local config = wezterm.config_builder()
-
 shells.apply_to_config(config)
 
 function get_appearance()
@@ -22,20 +21,18 @@ end
 
 config.color_scheme = scheme_for_appearance(get_appearance())
 
-config.font = wezterm.font_with_fallback({
-    "Cartograph CF",
-    "PragmataPro Mono",
-    "JetBrains Mono",
-})
+config.font = wezterm.font_with_fallback {
+  'Cartograph CF',
+  'PragmataPro Mono',
+  'JetBrains Mono',
+}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = true
 
-config.window_close_confirmation = "NeverPrompt"
+config.window_close_confirmation = 'NeverPrompt'
 
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    config.prefer_egl = false
-end
+config.term = 'wezterm'
 
 return config
