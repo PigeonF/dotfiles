eef62053b62eb4de2c1653fb96fadb8f87e3ce54
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 12:38:34 +0200

    chore(nixos): Enable wezterm to set some env variables

--- nixos/ssh.nix
+++ nixos/ssh.nix
@@ -8,6 +8,11 @@ _:
       {
         services.openssh.enable = lib.mkDefault true;
         services.openssh.openFirewall = lib.mkDefault true;
+        services.openssh.extraConfig = ''
+          AcceptEnv LANG LANGUAGE LC_*
+          AcceptEnv COLORTERM TERM TERM_*
+          AcceptEnv WEZTERM_REMOTE_PANE
+        '';
 
         security.sudo.extraConfig = ''
           Defaults env_keep+=SSH_AUTH_SOCK
