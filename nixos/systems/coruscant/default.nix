662bf6e5e1b6fd3f1fba39f3577a1234646ab0db
Jonas Fierlings <fnoegip@gmail.com>
2024-04-16 14:53:40 +0200

    chore(nix): Install socat on wsl

--- nixos/systems/coruscant/default.nix
+++ nixos/systems/coruscant/default.nix
@@ -1,23 +1,27 @@
 { inputs, ... }:
 
 {
-  flake.nixosModules.coruscant = _: {
-    imports = [
-      inputs.nixos-wsl.nixosModules.wsl
+  flake.nixosModules.coruscant =
+    { pkgs, ... }:
+    {
+      imports = [
+        inputs.nixos-wsl.nixosModules.wsl
 
-      inputs.self.nixosModules.core
-      inputs.self.nixosModules.home-manager
-      inputs.self.nixosModules.nix
-      inputs.self.nixosModules.pigeon
+        inputs.self.nixosModules.core
+        inputs.self.nixosModules.home-manager
+        inputs.self.nixosModules.nix
+        inputs.self.nixosModules.pigeon
 
-      inputs.self.nixosModules.vsCodeRemoteSSHFix
-    ];
+        inputs.self.nixosModules.vsCodeRemoteSSHFix
+      ];
 
-    networking.hostName = "coruscant";
+      networking.hostName = "coruscant";
 
-    wsl = {
-      enable = true;
-      defaultUser = "pigeon";
+      wsl = {
+        enable = true;
+        defaultUser = "pigeon";
+      };
+
+      environment.defaultPackages = [ pkgs.socat ];
     };
-  };
 }
