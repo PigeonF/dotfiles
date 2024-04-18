364c9703ff9e840bd7db7db883fcc12aa58d1d7f
Jonas Fierlings <fnoegip@gmail.com>
2024-04-18 11:08:01 +0200

    chore(nvim): Do not check for updates on start

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -420,6 +420,9 @@ require('lazy').setup({
 
   require 'kickstart.plugins.lint',
 }, {
+  checker = {
+    enabled = false
+  },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
     -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
