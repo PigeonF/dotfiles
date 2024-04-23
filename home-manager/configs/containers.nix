b92b978d6ccc703b68cc6aa042826c01dff4ace0
Jonas Fierlings <fnoegip@gmail.com>
2024-04-23 20:56:59 +0200

    feat: Make registry.internal accessible in buildx container

--- home-manager/configs/containers.nix
+++ home-manager/configs/containers.nix
@@ -16,6 +16,14 @@
 
     sessionVariables = {
       DOCKER_CONFIG = "${config.xdg.dataHome}/docker";
+      BUILDX_CONFIG = "${config.xdg.dataHome}/buildx";
     };
+  };
+
+  xdg.dataFile."buildx/buildkitd.default.toml" = {
+    text = ''
+      [registry."registry.internal"]
+        ca=["/etc/docker/certs.d/registry.internal/ca.crt"]
+    '';
   };
 }
--- nixos/dockerRegistry.nix
+++ nixos/dockerRegistry.nix
@@ -1,6 +1,27 @@
 {
   flake.nixosModules.dockerRegistry =
     { pkgs, config, ... }:
+    let
+      # Caddy creates the `.crt` file with 0600 permissions, which means it cannot be read when
+      # creating a new buildx builder. Instead, we create another self signed cert just for the
+      # registry.
+      #
+      # https://caddy.community/t/certificate-file-permissions-when-sharing-certificates/13211
+      certificate =
+        pkgs.runCommand "self-signed-certs-docker-registry" { buildInputs = [ pkgs.openssl ]; }
+          ''
+            mkdir $out
+
+            openssl ecparam -name prime256v1 -genkey -noout -out $out/registry.key
+            openssl req -new -x509 -key $out/registry.key -out $out/registry.crt -days 3600 \
+              -subj "/CN=Caddy Local Authority - 2024 ECC Registry" \
+              -addext "keyUsage = keyCertSign, cRLSign" \
+              -addext "basicConstraints=critical, CA:true, pathlen:1" \
+              -addext "subjectAltName = DNS:registry.internal"
+
+            chmod 0644 $out/registry.crt
+          '';
+    in
     {
       services.dockerRegistry = {
         enable = true;
@@ -10,12 +31,13 @@
       };
 
       services.caddy.virtualHosts."registry.internal".extraConfig = ''
-        tls internal
+        tls "${certificate}/registry.crt" "${certificate}/registry.key"
+
         reverse_proxy http://${config.services.dockerRegistry.listenAddress}:${toString config.services.dockerRegistry.port}
       '';
 
       environment.etc."docker/certs.d/registry.internal/ca.crt" = {
-        source = "${config.services.caddy.dataDir}/.local/share/caddy/certificates/local/registry.internal/registry.internal.crt";
+        source = "${certificate}/registry.crt";
       };
     };
 }
