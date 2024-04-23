9793eb4cfddaf4dee76101a31b5c74d58cb563bc
Jonas Fierlings <fnoegip@gmail.com>
2024-04-23 19:44:06 +0200

    feat: Load internal registry certificate in docker

--- nixos/dockerRegistry.nix
+++ nixos/dockerRegistry.nix
@@ -13,5 +13,9 @@
         tls internal
         reverse_proxy http://${config.services.dockerRegistry.listenAddress}:${toString config.services.dockerRegistry.port}
       '';
+
+      environment.etc."docker/certs.d/registry.internal/ca.crt" = {
+        source = "${config.services.caddy.dataDir}/.local/share/caddy/certificates/local/registry.internal/registry.internal.crt";
+      };
     };
 }
