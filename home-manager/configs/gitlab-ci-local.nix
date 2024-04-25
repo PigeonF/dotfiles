a2f1ca5a7476bd2f321f9439c61c6324751e2e9f
Jonas Fierlings <fnoegip@gmail.com>
2024-04-25 17:32:07 +0200

    chore(gcl): Add variables.yml

--- home-manager/configs/gitlab-ci-local.nix
+++ home-manager/configs/gitlab-ci-local.nix
@@ -15,21 +15,38 @@
     };
   };
 
-  xdg.configFile."gitlab-ci-local/.gitlab-ci-local/.env" = {
-    text =
-      let
-        volumes = lib.strings.concatStringsSep " " [
-          "builds:/builds"
-          "cache:/cache"
-          "certs:/certs/client"
-          "/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro"
-        ];
-        variables = lib.strings.concatStringsSep " " [ "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt" ];
-      in
-      ''
-        PRIVILEGED=true
-        VOLUME="${volumes}"
-        VARIABLE="${variables}"
+  xdg.configFile = {
+    "gitlab-ci-local/.gitlab-ci-local/.env" = {
+      text =
+        let
+          volumes = lib.strings.concatStringsSep " " [
+            "builds:/builds"
+            "cache:/cache"
+            "certs:/certs/client"
+            "/etc/ssl/certs/ca-certificates.crt:/etc/ssl/certs/ca-certificates.crt:ro"
+          ];
+          variables = lib.strings.concatStringsSep " " [ "SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt" ];
+        in
+        ''
+          PRIVILEGED=true
+          VOLUME="${volumes}"
+          VARIABLE="${variables}"
+        '';
+    };
+
+    "gitlab-ci-local/.gitlab-ci-local/variables.yml" = {
+      text = ''
+        ---
+        global:
+          CI: "true"
+          CI_API_GRAPHQ_URL: https://gitlab.com/api/graphql
+          CI_BUILDS_DIR: /gcl-builds
+          CI_CONFIG_PATH: .gitlab-ci.yml
+          CI_PIPELINE_SOURCE: "merge_request_event"
+          CI_MERGE_REQUEST_TARGET_BRANCH_NAME: main
+          GITLAB_USER_ID: 409429
+          GITLAB_USER_LOGIN: PigeonF
       '';
+    };
   };
 }
