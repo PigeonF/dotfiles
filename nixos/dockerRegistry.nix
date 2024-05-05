566379b52996f121b765e81186db145eeb0705c3
Jonas Fierlings <fnoegip@gmail.com>
2024-05-05 23:54:18 +0200

    fix: Fix registry settings

--- nixos/dockerRegistry.nix
+++ nixos/dockerRegistry.nix
@@ -15,15 +15,21 @@
     docker.daemon.settings."insecure-registries" = [ "registry.internal" ];
   };
 
-  services.nginx.virtualHosts."registry.internal".locations."/".proxyPass = "http://127.0.0.1:5000/";
+  services.nginx.virtualHosts."registry.internal".locations."/" = {
+    proxyPass = "http://127.0.0.1:5000/";
+    extraConfig = ''
+      client_max_body_size 0;
+    '';
+  };
 
   systemd.services."docker-network-dev".wantedBy = [ "docker-registry.internal.service" ];
 
   environment.etc."buildkit/buildkitd.toml" = {
     text = ''
+      debug = true
+
       [registry."registry.internal"]
         http = true
-        insecure = true
     '';
   };
 }
