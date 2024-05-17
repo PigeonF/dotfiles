9bb10ebd733956ba3a16bba429df1fe59e623323
Jonas Fierlings <fnoegip@gmail.com>
2024-05-17 11:17:22 +0200

    chore(nvim): Enable rust-analyzer

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -212,6 +212,7 @@ require('lazy').setup({
             },
           },
         },
+        rust_analyzer = {},
       }
 
       if not vim.loop.os_uname().sysname == 'Windows_NT' then
@@ -421,7 +422,7 @@ require('lazy').setup({
   require 'kickstart.plugins.lint',
 }, {
   checker = {
-    enabled = false
+    enabled = false,
   },
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
