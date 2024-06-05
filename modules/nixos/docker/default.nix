819908fd0fe4088315ab9c950c5d721c897269af
Jonas Fierlings <fnoegip@gmail.com>
2024-06-05 21:01:07 +0200

    Update

--- modules/nixos/docker/default.nix
+++ modules/nixos/docker/default.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 let
   cfg = config.pigeonf.docker;
 in
@@ -18,6 +23,36 @@ in
             containerd-snapshotter = true;
           };
           dns = [ "10.117.0.1" ];
+          seccomp-profile =
+            let
+              moby = builtins.fetchGit {
+                url = "https://github.com/moby/moby.git";
+                ref = "master";
+                rev = "ceefb7d0b9c5b6fbd1ea7511592a4ddb28ec4821";
+              };
+              mobySeccomp = builtins.fromJSON (builtins.readFile "${moby}/profiles/seccomp/default.json");
+
+              buildkitSeccompJson = builtins.toJSON (
+                mobySeccomp
+                // {
+                  syscalls = mobySeccomp.syscalls ++ [
+                    {
+                      names = [
+                        "clone"
+                        "keyctl"
+                        "mount"
+                        "pivot_root"
+                        "sethostname"
+                        "umount2"
+                        "unshare"
+                      ];
+                      action = "SCMP_ACT_ALLOW";
+                    }
+                  ];
+                }
+              );
+            in
+            pkgs.writeText "seccomp.json" buildkitSeccompJson;
         };
       };
     };
