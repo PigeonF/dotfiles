66122e42f0a1c204393827aa8cfa15c642f34125
Jonas Fierlings <fnoegip@gmail.com>
2024-06-07 11:20:54 +0200

    Update gitlab-ci-local

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,7 +13,10 @@ let
     buildah = _: prev: { inherit (nixos-unstable-small.legacyPackages.${prev.system}) buildah; };
     git-cliff = final: _: { git-cliff = final.callPackage ./git-cliff { }; };
     gitlab-ci-local = _: prev: {
-      inherit (nixpkgs-gitlab-ci-local.legacyPackages.${prev.system}) gitlab-ci-local;
+      gitlab-ci-local =
+        nixpkgs-gitlab-ci-local.legacyPackages.${prev.system}.gitlab-ci-local.overrideAttrs
+          (_: prev: { patches = (prev.patches or [ ]) ++ [ ./gitlab-ci-local/pr-1258.patch ]; });
+      # inherit (nixpkgs-gitlab-ci-local.legacyPackages.${prev.system}) gitlab-ci-local;
     };
     just = _: prev: { inherit (nixpkgs-just.legacyPackages.${prev.system}) just; };
     neovim = _: prev: {
