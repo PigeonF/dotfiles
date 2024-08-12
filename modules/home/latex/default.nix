880cbe62c236952f6119a1046869d3e564974e1e
Jonas Fierlings <fnoegip@gmail.com>
2024-08-12 15:03:36 +0200

    chore: Add latex configuration

---
+++ modules/home/latex/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.pigeonf.latex;
+  inherit (lib) mkIf mkEnableOption;
+in
+{
+  _file = ./default.nix;
+
+  options.pigeonf.latex = {
+    enable = mkEnableOption "PigeonF LaTeX Packages";
+  };
+
+  config = mkIf cfg.enable {
+    home = {
+      packages = [ (pkgs.texlive.combine { inherit (pkgs.texlive) scheme-medium latex-bin latexmk; }) ];
+    };
+  };
+}
