abf7c0465aff6627c3d994cea336b483adf50869
Jonas Fierlings <fnoegip@gmail.com>
2024-09-05 16:14:32 +0200

    incus: Resolve container ips by name

--- modules/nixos/incus/default.nix
+++ modules/nixos/incus/default.nix
@@ -11,8 +11,55 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    virtualisation.incus.enable = true;
-    networking.firewall.trustedInterfaces = [ "incusbr0" ];
-  };
+  config =
+    let
+      ip = "10.109.165.1";
+    in
+    lib.mkIf cfg.enable {
+      virtualisation.incus = {
+        enable = true;
+        preseed = {
+          networks = [
+            {
+              config = {
+                "ipv4.address" = "${ip}/24";
+                "ipv4.nat" = "true";
+              };
+              name = "incusbr0";
+              type = "bridge";
+            }
+          ];
+          profiles = [
+            {
+              devices = {
+                eth0 = {
+                  name = "eth0";
+                  network = "incusbr0";
+                  type = "nic";
+                };
+                root = {
+                  path = "/";
+                  pool = "default";
+                  size = "35GiB";
+                  type = "disk";
+                };
+              };
+              name = "default";
+            }
+          ];
+          storage_pools = [
+            {
+              config = {
+                source = "/var/lib/incus/storage-pools/default";
+              };
+              driver = "btrfs";
+              name = "default";
+            }
+          ];
+        };
+      };
+      networking.firewall.trustedInterfaces = [ "incusbr0" ];
+
+      services.dnsmasq.settings.server = [ "/incus/${ip}" ];
+    };
 }
