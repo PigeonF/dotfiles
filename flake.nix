9979d6b735e383978743661ca8cf165db32958de
Jonas Fierlings <fnoegip@gmail.com>
2025-12-01 20:46:39 +0100

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Nix configurations for my dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=refs/heads/release-25.11";
+    systems.url = "github:nix-systems/default?ref=refs/heads/main";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts?ref=refs/heads/main";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      systems,
+      ...
+    }:
+    flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      (_: {
+        _file = ./flake.nix;
+
+        systems = import systems;
+
+        imports = [ ];
+
+        flake = { };
+
+        perSystem = { };
+      });
+}
