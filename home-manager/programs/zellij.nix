cc16746a3c1376e219e29298c1ed37c55790c607
Jonas Fierlings <fnoegip@gmail.com>
2026-01-01 10:39:13 +0100

    zellij: Add shell function for refreshing SSH_AUTH_SOCK
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- home-manager/programs/zellij.nix
+++ home-manager/programs/zellij.nix
@@ -29,5 +29,22 @@ in
         };
       };
     }
+    (lib.mkIf cfg.enable {
+      programs = {
+        bash = {
+          initExtra = ''
+            # Refresh the SSH_AUTH_SOCK variable from within zellij
+            # See <https://github.com/zellij-org/zellij/issues/1637>
+            function zellij-refresh-ssh-sock() {
+              if [ -n "$ZELLIJ" ]; then
+                if SSH_AUTH_SOCK=$(find "$HOME/.ssh/agent" -type s -exec ls -1rt "{}" + | head -n 1); then
+                  export SSH_AUTH_SOCK
+                fi
+              fi
+            }
+          '';
+        };
+      };
+    })
   ];
 }
