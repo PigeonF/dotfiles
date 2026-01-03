a244f7ea96e0bc649bd8aeab4ac5a7d4f6f2120d
Jonas Fierlings <fnoegip@gmail.com>
2026-01-03 11:32:29 +0100

    Add comments to wezterm config
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,15 +2,18 @@ local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 config:set_strict_mode(true)
 
+-- Color Scheme
 config.color_scheme = "Catppuccin Macchiato"
 local scheme = wezterm.color.get_builtin_schemes()[config.color_scheme]
 -- Bright green is unreadable with white text
 scheme.ansi[3] = "#8FBA84"
 config.colors = scheme
 
+-- Font
 config.font = wezterm.font("JetBrainsMono NFM")
 config.font_size = 17
 
+-- Launch Menu
 config.launch_menu = {}
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { "pwsh.exe", "-NoLogo" }
@@ -18,6 +21,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     table.insert(config.launch_menu, { label = "PowerShell", args = {"pwsh.exe", "-NoLogo"}, cwd = "D:\\" })
 end
 
+-- Tab Bar
 config.use_fancy_tab_bar = false
 config.tab_max_width = 21
 config.hide_tab_bar_if_only_one_tab = true
