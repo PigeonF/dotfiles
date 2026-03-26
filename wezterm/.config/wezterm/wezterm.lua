2abe3a9baca79a2b6531fa9c7cf0dec3d7c411f9
Jonas Fierlings <fnoegip@gmail.com>
2026-03-26 19:04:18 +0100

    wezterm: Load visual studio tools on windows
    Signed-off-by: Jonas Fierlings <fnoegip@gmail.com>

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -16,7 +16,7 @@ config.font_size = 13
 -- Launch Menu
 config.launch_menu = {}
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-    config.default_prog = { "pwsh.exe", "-NoLogo" }
+    config.default_prog = { "pwsh.exe", "-NoLogo", "-NoExit", "-Command", "& 'C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\Tools\\Launch-VsDevShell.ps1'" }
     config.default_cwd = "D:\\"
     table.insert(config.launch_menu, { label = "PowerShell", args = {"pwsh.exe", "-NoLogo"}, cwd = "D:\\" })
 end
